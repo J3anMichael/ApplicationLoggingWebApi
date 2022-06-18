ae5b5e95122e4c927c65e95286cfe9c1548de38e
Jean Michael <jeanmmclaudino@gmail.com>
2022-06-17 23:40:15 -0300

    Add project files.

---
+++ Controllers/LoggerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApplicationLoggingWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoggerController : ControllerBase
+    {
+        private readonly ILogger<LoggerController> _logger;
+        public LoggerController(ILogger<LoggerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            _logger.LogTrace("Log information message.");
+            _logger.LogDebug("Log information message.");
+            _logger.LogInformation("Log information message.");
+            _logger.LogWarning("Cuidado aí garotinho.");
+            _logger.LogError("Deu ruim deu ruim.");
+            _logger.LogCritical("A casa caiu, #Lascou!!");
+
+
+            return Ok();
+        }
+    }
+}
